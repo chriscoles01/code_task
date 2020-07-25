6455a1460e4e64f7434b43c53b59813dd708cd57
Christopher Coles <cbc1g16@soton.ac.uk>
2020-07-25 14:39:44 +0200

    [rework] simplify conflicted function to return int

--- Task_1/src/Utilities/genome_utlities.py
+++ Task_1/src/Utilities/genome_utlities.py
@@ -6,24 +6,24 @@ class GenomeUtilities:
     def GenerateConstraintGraph(genome_DataFrame : pd.DataFrame) -> pd.DataFrame:
         conflict_DataFrame = pd.DataFrame(index=genome_DataFrame.index, columns=genome_DataFrame.index)
         for region_id_1 in genome_DataFrame.index:
+            # conflict_DataFrame[region_id_1] = genome_DataFrame.apply(lambda row: GenomeUtilities.IsConflicted(row, genome_DataFrame.loc[region_id_1]), axis=1)
             for region_id_2 in genome_DataFrame.index:
-                compatible = 1
-                if region_id_1 != region_id_2:
-                    region_1 = genome_DataFrame.loc[genome_DataFrame.index == region_id_1]
-                    region_2 = genome_DataFrame.loc[genome_DataFrame.index == region_id_1]
-                    
-                    if GenomeUtilities.IsConflicted(region_1, region_2):
-                        compatible = 0    
+                region_1 = genome_DataFrame.loc[region_id_1]
+                region_2 = genome_DataFrame.loc[region_id_2]
+                
+                compatible = GenomeUtilities.IsConflicted(region_1, region_2)
 
                 conflict_DataFrame[region_id_1][region_id_2] = compatible
         
         return conflict_DataFrame
 
     @staticmethod   
-    def IsConflicted(region_1 : pd.DataFrame, region_2 : pd.DataFrame) -> bool:
-        if (region_1.end < region_2.start):
-            return False
-        elif (region_1.start > region_2.end):
-            return False
+    def IsConflicted(region_1 : pd.Series, region_2 : pd.DataFrame) -> int:
+        if region_1.equals(region_2):
+            return 1
+        if (region_1['end'] < region_2.start):
+            return 0
+        elif (region_1['start'] > region_2.end):
+            return 0
         else:
-            return True
+            return 1
