712637d3f64d65f67e0b5fa24a709bbe85efa39f
Christopher Coles <cbc1g16@soton.ac.uk>
2020-07-25 13:18:29 +0200

    [add] return type on genome parser

--- Task_1/src/Utilities/GenomeParser.py
+++ Task_1/src/Utilities/GenomeParser.py
@@ -3,7 +3,7 @@ import pandas as pd
 class GenomeParser:
     
     @staticmethod
-    def ToGenomeDataFrame(path : str):
+    def ToGenomeDataFrame(path : str) -> pd.DataFrame:
         genome_DataFrame = pd.read_csv(path, sep='\t', names=['start','end'])
         return genome_DataFrame
 
