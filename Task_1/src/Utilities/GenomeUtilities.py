7d234d13bfc57a73c7a98a7e1ad91745a7d25c45
Christopher Coles <cbc1g16@soton.ac.uk>
2020-07-25 13:57:12 +0200

    [add] logic compute region conflicts in a dataframe

--- Task_1/src/Utilities/GenomeUtilities.py
+++ Task_1/src/Utilities/GenomeUtilities.py
@@ -4,9 +4,22 @@ class GenomeUtilities:
 
     @staticmethod
     def GenerateConstraintGraph(genome_DataFrame : pd.DataFrame) -> pd.DataFrame:
-        pass
-    
-    @staticmethod
+        conflict_DataFrame = pd.DataFrame(index=genome_DataFrame.index, columns=genome_DataFrame.index)
+        for region_id_1 in genome_DataFrame.index:
+            for region_id_2 in genome_DataFrame.index:
+                compatible = 1
+                if region_id_1 != region_id_2:
+                    region_1 = genome_DataFrame.loc[genome_DataFrame.index == region_id_1]
+                    region_2 = genome_DataFrame.loc[genome_DataFrame.index == region_id_1]
+                    
+                    if GenomeUtilities.IsConflicted(region_1, region_2):
+                        compatible = 0    
+
+                conflict_DataFrame[region_id_1][region_id_2] = compatible
+        
+        return conflict_DataFrame
+
+    @staticmethod   
     def IsConflicted(region_1 : pd.DataFrame, region_2 : pd.DataFrame) -> bool:
         if (region_1.end < region_2.start):
             return False
