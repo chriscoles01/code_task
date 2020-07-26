bbccc7f252ab033b2bbcb332eb56890fd5d6bee0
Christopher Coles <cbc1g16@soton.ac.uk>
2020-07-26 21:57:08 +0200

    [add] genome reader to task 2

---
+++ Task_2/src/Utilities/genome_parser.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+class GenomeParser:
+    
+    @staticmethod
+    def read_genome_dataframe(path : str) -> pd.DataFrame:
+        genome_DataFrame = pd.read_csv(path, sep='\t', names=['start','end'])
+        return genome_DataFrame
+
